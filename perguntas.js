a7fdb2be7f6cafe780edd5c1d14b1de62491ae16
Vitória Lims <00001078831087sp@al.educacao.sp.gov.br>
2024-11-29 13:42:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Normalmente, quantos litros de sangue uma pessoa tem?',
+    'Entre 4 a 6 litros. O volume de sangue é entre 7% e 8% o peso corporal.'
+)
+
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo"?',
+    'Descartes. Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
+)
+
+criaCartao(
+    'Inovação',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Brasil. Francisco Canhos é o nome do homem que, na década de 40, desenvolveu o primeiro chuveiro elétrico seguro em Jaú-SP. '
+)
+
+criaCartao(
+    'Gerais',
+    'Quanto tempo a luz do Sol demora para chegar à Terra??',
+    '8 minutos. Ela é calculada com base na distância do Sol em relação à Terra, que é de aproximadamente 150 000 000 km, considerando que a velocidade da luz no vácuo é 300 000 Km/s.'
+)
